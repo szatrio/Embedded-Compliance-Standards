f60e6f00bfa4aca70cb529f5e04af03ead038de2
Satrio Utomo <szatrio@gmail.com>
2026-05-12 22:14:38 +0700

    initial commit

---
+++ MISRA-C-2012/Rule-04.06.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+
+void test_compliance_4_6(int parameter_ngaco) {
+
+    long nilai_sensor = 1000L; 
+    char status = 'A';
+
+    if (nilai_sensor > 0) {
+        nilai_sensor--;
+    }
+}
